28a0b848feaac2693a7e8000a7ef3c8604892c66
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-30 10:06:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
+global.owner = ['2347039570336'] //ur owner number
+global.ownernomer = "2347039570336" //ur owner number2
 global.ownername = "🦄Dream Guy Xeon" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "nigeria, lagos, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
+global.botname = "GIM Bot  V8"
+global.ownernumber = '2347039570336'
 global.ownername = '🦄드림 가이 Xeon'
 global.ownerNumber = ["916909137213@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
